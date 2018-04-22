c2a51fa50fba0b67fda37b5a986d213fb82f8a51
root <root@ippbx.buero.schneiders-shk.de>
2018-04-22 12:27:23 +0200

    CID + Area code lookup working

--- cid_lookup_server.py
+++ cid_lookup_server.py
@@ -5,7 +5,7 @@ import socketserver
 from urllib.parse import parse_qs
 import pandas
 import re
-import sqlite
+import sqlite3
 
 
 PORT = 8000
@@ -15,27 +15,38 @@ class Serv(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         params = parse_qs(self.path[2:])
         number = params["num"][0]
-        self.wfile.write("Sie suchten nach der Telefonnummer: {}".format(number).encode())
-        city = self.code_lookup(int(number))
-        print(city)
-        
+        city = self.code_lookup(number)
+        name = self.cid_lookup(number)
+        self.wfile.write(name.encode())
+        self.wfile.write("\n".encode())
         self.wfile.write(city.encode())
 
 
-    def code_lookup(self, code):
+    def code_lookup(self, cid):
         if not "csv" in globals():
             self.csv = pandas.read_csv("codes.csv", sep=";", index_col=0).T.to_dict()
 
-        if code in self.csv.keys():
-            return str(self.csv[code]["city"])
-        else:
-            return "Unknown city"
 
-    def cid_lookup(self, did):
-        if not "db" in globals():
+        cid = cid[1:]
+        for i in range(1, len(cid)):
+            code = cid[:i]
+            if int(code) in self.csv.keys():
+                return str(self.csv[int(code)]["city"])
+        return "Unknown city"
+
+    def cid_lookup(self, cid):
+        if not "cur" in globals():
             self.db = sqlite3.connect("/var/lib/asterisk/astdb.sqlite3")
-        db.execute("SELECT * FROM astdb;")
+            self.cur = self.db.cursor()
+        self.cur.execute("SELECT value FROM astdb WHERE key='/cidname/{}';".format(cid))
 
+        cid_name = ""
+        try:
+             cid_name = self.cur.fetchall()[0][0]
+        except:
+             cid_name = "Unbekannt"
+
+        return cid_name
 
 
 
